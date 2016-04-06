b171d635b43ff69ec5045d9188bd82350a1a046a
linhaobuaa <1257819385@qq.com>
2016-04-06 22:36:19 +0800

    zhang

--- user_portrait/user_portrait/static/js/portrait/net.js
+++ user_portrait/user_portrait/static/js/portrait/net.js
@@ -7,6 +7,7 @@ function call_sync_ajax_request(url, callback){
       success:callback
     });
 }
+
 var myDate = new Date();
 var month = myDate.getMonth()+1;
 var day = myDate.getDate();
@@ -16,20 +17,37 @@ var mm =myDate.getMinutes();
 var count_hh = Math.floor(hh/3);
 var show_hh = [];
 for(var i=0;i<count_hh;i++){
-	show_hh.push(i*3);
+	show_hh.push((i+1)*3);
 }
+var change_period = '';
+var tab = 'time';
 date_init();
+var task_id = '';
+
 function hidden_keywords(){
     $('#show_keywords').addClass('hidden');
 	$('#by_keywords').css('background-color','#6699FF');
 	$('#framecontent').removeClass('hidden');
 	$('#by_time').css('background-color','#3351B7');
+	$('#pr_diff').removeClass('hidden');
+	$('#dg_diff').removeClass('hidden');
+	tab = 'time';
+	$("input[name='mode_choose']").eq(0).attr("checked","checked");
+	var url = '/network/show_daily_rank/'
+    call_sync_ajax_request(url, temporal_rank_table);
 }
 function hidden_time(){
     $('#framecontent').addClass('hidden');
+	$('#pr_diff').addClass('hidden');
+	$('#dg_diff').addClass('hidden');
 	$('#by_time').css('background-color','#6699FF');
     $('#show_keywords').removeClass('hidden');
 	$('#by_keywords').css('background-color','#3351B7');
+	tab = 'keyword';
+	$("input[name='mode_choose']").eq(0).attr("checked","checked");
+	var url = '/network/show_daily_rank/'
+    call_sync_ajax_request(url, temporal_rank_table);
+	
 }
 function date_init(){
     var date = choose_time_for_mode();
@@ -56,12 +74,110 @@ function date_init(){
     }
     var real_date = new Date();
     real_date = real_date.format('yyyy/MM/dd');
-    console.log(real_date);
     $('#search_date #weibo_modal').datetimepicker({value:real_date,step:1440,format:'Y/m/d',timepicker:false});
 
 }
-function submit_detect(){
-	console.log('asdsfsfsd');
+
+//任务状态
+var url = '/network/search_all_keywords/';
+call_sync_ajax_request(url, detect_task_status);
+function detect_task_status(data) {
+    var html = '';
+    html += '<span style="float:right;margin-right:0px;margin-bottom: 10px;cursor:pointer;" type="button" data-toggle="modal" data-target="#detect_search_modal" ><u>任务搜索</u></span>';
+    html += '<span id="show_all_task" style="float:right;margin-right: 20px;margin-bottom: 10px;cursor:pointer;"><u>显示全部任务</u></span>';
+    if (data != ''){
+        var sort_scope = data.sort_scope;
+        $('#detect_task_status').empty();
+        html += '<br><table id="task_table" class="table table-bordered table-striped table-condensed datatable" style="margin-left:30px;width:900px;">';
+        html += '<thead>';
+        html += '<th style="width:100px;text-align:center;">关键词</th>';
+        html += '<th style="width:150px;text-align:center;">监控时间</th>';
+        html += '<th style="width:200px;text-align:center;">提交时间</th>';
+        html += '<th style="width:100px;text-align:center;">任务状态</th>';
+        html += '<th style="width:60px;text-align:center;">操作</th>';
+        html += '</thead>';
+        for(var i=0;i<data.length;i++){
+            var delete_this = '<span style="display:none;">'+data[i][0]+'</span><span class="de_delete_this"><b><u class="" style="cursor:pointer;">删除</u></b></span>';
+            if(data[i][5] == 0){
+                var status = '正在计算';
+            }else{
+                var status = '<span class="show_detect_key_result" ><b><u style="cursor:pointer;">计算完成</u></b></span>';
+            }
+            html += '<tr>';
+            html += '<td style="text-align:center;">'+data[i][3].split('&').join(',')+'</td>';
+            html += '<td style="text-align:center;">'+data[i][1]+' 至 '+data[i][2]+'</td>';
+            html += '<td style="text-align:center;">'+data[i][4]+'</td>';
+            html += '<td style="text-align:center;"><span style="display:none;">'+data[i][0]+'</span>'+status+'</td>';
+            html += '<td style="text-align:center;">'+delete_this+'</td>';
+            html += '</tr>';
+        }
+        html += '</table>';
+        $('#detect_task_status').append(html);
+    }else{
+        $('#task_table').css('display', 'none');
+        var html = '<div style="text-align: center;background-color: #cccccc;width: 900px;margin-left: 30px">暂无相关任务</div>'
+        $('#detect_task_status').append(html);
+    }
+}
+//显示全部任务
+$('#show_all_task').live('click', function(){
+    var task_url_all = '/network/search_all_keywords/';
+    call_sync_ajax_request(task_url_all, detect_task_status);
+});
+//搜索任务提交
+function search_task(){
+    var submit_date = $('#weibo_modal').val().split('/').join('-');
+    var start_date = $('#weibo_from_modal').val().split('/').join('-');
+    var end_date = $('#weibo_to_modal').val().split('/').join('-');
+    var submit_key = $('#search_key').val();
+    var search_url = '/network/search_all_keywords/';
+	var count = 0
+    if(submit_key != ''){
+        if(count ==0){
+			search_url += '?keywords='+submit_key;
+			count += 1;
+		}
+		else{
+			search_url += '&keywords='+submit_key;
+		}
+    }
+    var status = $('#search_status').val();
+    console.log(status);
+    if(status != "2"){
+		if(count ==0){
+			search_url += '?status=' +status;
+		}else{
+			search_url += '&status=' +status;
+			count += 1;
+		}
+        
+    };
+    var status= $('')
+    if($('#time_checkbox').is(':checked')){
+		if(count ==0){
+			search_url += '?start_date='+start_date+'&end_date='+end_date;
+		}else{
+			search_url += '&start_date='+start_date+'&end_date='+end_date;
+			count += 1;
+		}
+       
+    };
+    if($(' #time_checkbox_submit').is(':checked')){
+		if(count ==0){
+			search_url += '?submit_date='+submit_date;
+		}else{
+			search_url += '&submit_date='+submit_date;
+			count += 1;
+		}
+        
+    }
+    console.log(search_url);
+
+    call_sync_ajax_request(search_url, detect_task_status);
+}
+//提交监测
+$('#detect_submit').click(function(){
+	console.log('sas');
     var s = [];
     var keyword = $('#keyword_detect').val();
     var time_from =$('#detect_time_choose #weibo_from').val().split('/').join('-');
@@ -75,8 +191,14 @@ function submit_detect(){
     //console.log(keyword);
     if(keyword == ''){  //检查输入词是否为空
         alert('请输入关键词！');
+    }else{
+		var submit_url = '/network/submit_network_keywords/?keywords='+keyword+'&start_date='+time_from+'&end_date='+time_to;
+		call_sync_ajax_request(submit_url);
+        var show_url = '/network/search_all_keywords/';
+        call_sync_ajax_request(show_url, detect_task_status);
     }
-}
+})
+
 
 $(function(){
     var url = '/network/show_daily_rank/'
@@ -84,6 +206,24 @@ $(function(){
 });
 //画表
 function temporal_rank_table(data){
+    var box = document.getElementsByName('mode_choose');
+	for(var i=0;i<box.length;i++){
+		if(box[i].checked){
+			sort_type = box[i].value;
+		}
+	}
+	if(sort_type=='pr'){
+		var show_title = 'pagerank';
+	}
+	if(sort_type=='pr_diff'){
+		var show_title = 'pagerank变动';
+	}
+	if(sort_type=='dg'){
+		var show_title = '节点度';
+	}
+	if(sort_type=='dg_diff'){
+		var show_title = '节点度变动';
+	}
 	$('#result_rank_table').empty();
 	var html = '';
 	html += '<table id="rank_table" class="table table-striped table-bordered bootstrap-datatable datatable responsive" style="margin-left:30px;width:900px;">';
@@ -94,8 +234,8 @@ function temporal_rank_table(data){
 	html += '<th style="text-align:center;">注册地</th>';
 	html += '<th style="text-align:center;">粉丝数</th>';
 	html += '<th style="text-align:center;">微博数</th>';
-	html += '<th style="text-align:center;">pagerank</th></thead>';
-	
+	html += '<th style="text-align:center;">'+show_title+'</th>';
+	html += '<th style="text-align:center;">网络详情</th></thead>';
 	for(var i=0;i<data.length;i++){
 		var uid = data[i][0];
 		var uname = data[i][1];
@@ -130,6 +270,7 @@ function temporal_rank_table(data){
 		html += '<td style="text-align:center;">'+data[i][4]+'</td>';
 		html += '<td style="text-align:center;">'+data[i][2]+'</td>';
 		html += '<td style="text-align:center;">'+data[i][5]+'</td>';
+		html += '<td style="text-align:center;"><a data-toggle="modal" data-target="#detail_network" >查看网络详情</a></td>';
 		html += '</tr>';
 	}
 	html += '</table>';
@@ -177,6 +318,19 @@ function show_trend(data){
 					color: '#808080'
 				}]
 			},
+			plotOptions:{
+				series:{
+					cursor:'pointer',
+					events:{
+						click:function(event){
+							//point2weibo(event.point.x, trend[event.point.x]);
+							change_period = event.point.x;
+							var table_url = '/network/show_daily_rank/?period='+event.point.x;
+							call_sync_ajax_request(table_url,temporal_rank_table);
+						}
+					}
+				}
+			},
 			legend: {
 				layout: 'vertical',
 				align: 'right',
@@ -193,63 +347,55 @@ function show_trend(data){
 
 
 
-//提交监控
-function submit_detect(){
-    var s = [];
-
-    var show_arg = $('#detect_choose_detail_2 option:selected').text();
-    var keyword = $('#keyword_detect').val();
-
-    var arg = $('#detect_choose_detail_2 option:selected').val();
-    scope_arg = arg; 
-
-    var time_from =$('#detect_time_choose #weibo_from').val().split('/').join('-');
-    var time_to =$('#detect_time_choose #weibo_to').val().split('/').join('-');
-    var from_stamp = new Date($('#detect_time_choose #weibo_from').val());
-    var end_stamp = new Date($('#detect_time_choose #weibo_to').val());
-    if(from_stamp > end_stamp){
-        alert('起始时间不得大于终止时间！');
-        return false;
-    }
-    //console.log(keyword);
-}
-
 //离线任务删除
+$('.de_delete_this').live('click',function(){
+    var a = confirm('确定要删除吗？');
+    if (a == true){
+        var id= $(this).prev().text();
+        var del_url = '/network/delete_network_keywords/?task_id='+id;
+        call_sync_ajax_request(del_url,de_del);
+    }
+});
+
+
 function de_del(data){
-    console.log(data);
     if(data == true){
         alert('删除成功！');
-        var task_url = '/sentiment/search_sentiment_all_keywords_task/?submit_user='+username;
+        var task_url = '/network/search_all_keywords/';
         call_sync_ajax_request(task_url, detect_task_status);
     }else{
         alert('删除失败，请再试一次！');
     }
 }
-//搜索任务提交
-function search_task(){
-    var submit_date = $('#weibo_modal').val().split('/').join('-');
-    var start_date = $('#weibo_from_modal').val().split('/').join('-');
-    var end_date = $('#weibo_to_modal').val().split('/').join('-');
-    var submit_key = $('#search_key').val();
-    var search_url = '/sentiment/search_sentiment_all_keywords_task/?submit_user='+username;
-    if(submit_key != ''){
-        search_url += '&keywords='+submit_key;
-    }
-    //var status = $('input[name="search_status"]:checked').val();
-    var status = $('#search_status').val();
-    console.log(status);
-    if(status != "2"){
-        search_url += '&status=' +status;
-    };
 
-    var status= $('')
-    if($('#time_checkbox').is(':checked')){
-       search_url += '&start_date='+start_date+'&end_date='+end_date;
-    };
-    if($(' #time_checkbox_submit').is(':checked')){
-        search_url += '&submit_date='+submit_date;
-    }
-    console.log(search_url);
 
-    call_sync_ajax_request(search_url, detect_task_status);
-}
+$(function(){
+	$('#modechoose').click(function(){
+	var box = document.getElementsByName('mode_choose');
+	for(var i=0;i<box.length;i++){
+		if(box[i].checked){
+			sort_type = box[i].value;
+		}
+	}
+	if(tab=='time'){
+		if(change_period==''){
+			var url = "/network/show_daily_rank/?order="+sort_type;
+		}else{
+			var url = '/network/show_daily_rank/?order='+sort_type+'&period='+change_period;
+		}
+	}
+	if(tab=='keyword'){
+		var url = "/network/show_keywords_rank/?order="+sort_type+"&task_id="+task_id;
+	}
+	console.log(url);
+    call_sync_ajax_request(url,temporal_rank_table);
+	});
+});
+
+//完成计算
+$('.show_detect_key_result').live('click', function(){
+    task_id= $(this).prev().text();
+    var show_url = '/network/show_keywords_rank/?task_id=' + task_id;
+    call_sync_ajax_request(show_url, temporal_rank_table)
+});
+
